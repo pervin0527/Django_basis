6ad5c7d8baeb48e03355b2e2bcf2b39f5561497c
pervin0527 <zfbtldnz@gmail.com>
2024-03-25 11:48:27 +0900

    Updated 2024.03.25

---
+++ 03_about_template/first_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def example_view(request):
+    ## 명시한 first_app 디렉토리는 project 하위의 app 폴더 이름을 참조하는 것이 아님.
+    return render(request, "first_app/example.html") ## first_app/templates/first_app/example.html
+
+def variable_view(request):
+    my_var = {"first_name" : "Kim", 'last_name' : "minjae",
+              'some_list' : [1, 2, 3, 4], 
+              'some_dict' : {'inside_key' : 'inside_value'}}
+
+    return render(request, 'first_app/variable.html', context=my_var)
