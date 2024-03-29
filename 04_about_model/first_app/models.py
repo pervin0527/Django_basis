f3e0293ecb25b362e4740b36013a810d9f68c501
pervin0527 <zfbtldnz@gmail.com>
2024-03-29 11:43:25 +0900

    Updated 2024.03.29

--- 04_about_model/first_app/models.py
+++ 04_about_model/first_app/models.py
@@ -6,3 +6,6 @@ class FirstClass(models.Model): ## Define Table
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     age = models.IntegerField()
+
+    def __str__(self):
+        return f"First Name : {self.first_name}, Last Name : {self.last_name}, Age : {self.age}"
