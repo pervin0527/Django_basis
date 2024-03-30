8aa6ea06a815a9cf8f12afab9efff60665d2f735
pervin0527 <zfbtldnz@gmail.com>
2024-03-30 16:44:29 +0900

    Updated 2024.03.30

--- 04_about_model/first_app/models.py
+++ 04_about_model/first_app/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class FirstClass(models.Model): ## Define Table
     ## Define Attributes
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    age = models.IntegerField()
+    age = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
+    height = models.IntegerField(default=160, validators=[MinValueValidator(120), MaxValueValidator(300)]) ## default=0, null=True
 
     def __str__(self):
-        return f"First Name : {self.first_name}, Last Name : {self.last_name}, Age : {self.age}"
+        return f"First Name : {self.first_name}, Last Name : {self.last_name}, \
+                 Age : {self.age}, Height : {self.height}"
--- 04_about_model/first_app/views.py
+++ 04_about_model/first_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
+def user_list_view(request):
+    all_users = models.FirstClass.objects.all()
+
+    return render(request, 'first_app/all_users.html', context={'whole_users' : all_users})
