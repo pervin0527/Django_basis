e7b3172bdc53345edb3f8346cb5028b38b556581
pervin0527 <zfbtldnz@gmail.com>
2024-04-04 09:15:39 +0900

    Updated 2024.04.04

--- 07_about_CBM/first_app/urls.py
+++ 07_about_CBM/first_app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import home_view, HomeView, ThankView, ContactFormView
+from .views import home_view, HomeView, ThankView, ContactFormView, TeacherCreateView, TeacherListView
 
 app_name = 'first_app'
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('thank_you/', ThankView.as_view(), name="thank_you"),
     path('contact/', ContactFormView.as_view(), name='contact'),
+    path('create_teacher/', TeacherCreateView.as_view(), name='create_teacher'),
+    path('teachers_list/', TeacherListView.as_view(), name='teachers_list')
+
 ]
--- 07_about_CBM/first_app/views.py
+++ 07_about_CBM/first_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, FormView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, FormView, CreateView, ListView
 from .forms import ContactForm
+from .models import Teacher
 
 # Create your views here.
-
 def home_view(request):
     return render(request, 'first_app/home.html')
 
@@ -25,3 +26,19 @@ class ContactFormView(FormView):
         print(form.cleaned_data)
 
         return super().form_valid(form) ## FormView 부모 클래스에 정의된 form_valid 메서드를 호출.
+    
+class TeacherCreateView(CreateView):
+    model = Teacher ## CreateView는 model 클래스 이름을 통해 "template_name"을 결정한다. 즉, first_app/teacher_form.html을 템플릿으로 찾으려 한다.
+    fields = "__all__"
+
+    ## 유효성 검사와 저장(.save())를 자동으로 수행한다.
+    success_url = reverse_lazy('first_app:teachers_list')
+
+class TeacherListView(ListView):
+    ## model_list.html
+    model = Teacher
+    context_object_name = "teachers_list" ## default : object_list
+
+    ## ListView의 기본작업은 model.objects.all()이지만 queryset을 커스터마이징 할 수도 있다.
+    # queryset = Teacher.objects.all() ## default
+    queryset = Teacher.objects.order_by('first_name') ## Query를 커스터마이징 하거나, 필터링을 적용할 수 있음.
